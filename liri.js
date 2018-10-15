da7c2bb97a0bd48c612f2e0f00a3692f6b58afb3
Ian Richard <ianrichard03@gmail.com>
2018-10-14 22:29:49 -0500

    added comments.

--- liri.js
+++ liri.js
@@ -1,13 +1,15 @@
 //import modules
-var envv = require("dotenv").config();
+require("dotenv").config(); //what does this do?
 var keys = require("./key.js");
+
+//assign process argument values to an array.
 var parameterArray = [];
-
-var spotify = new Spotify(keys.spotify);
-
-
 process.argv.forEach(element => {
     parameterArray.push(element);
 });
+
+//Create Spotify object
+var spotify = new Spotify(keys.spotify);
+
 
 console.log(spotify);
